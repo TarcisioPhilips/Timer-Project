5d3b030a0707a2323463f4b7cb5646d28219168f
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-26 23:30:08 -0300

    chore: styled-components test with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "./components/Button";
+
 export function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+    </div>
   )
 }
 
